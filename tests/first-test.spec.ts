101dda191ab72d7f0589e6662acc4f0ed254999e
Eli Green <egreen@symetrix.co>
2022-03-07 11:48:50 -0800

    Changed playwright to run symetrix.co tests

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -1,24 +1,16 @@
 import {test, expect, Page} from '@playwright/test';
 
 test.beforeEach(async({page}) => {
-    await page.goto('localhost:8080');
+    await page.goto('https://www.symetrix.co/');
 })
 
 
 test('example test', async ({page}) => {
-  await page.type("#name", "Eli");
-  await page.type("#mail", "myemail@123.com");
-  await page.type("#pwd", "Password");
-
-  await page.selectOption("#card", "mc");
-  await page.type("#number", "1234123412341234");
-  await page.type("#expiration", "03/03");
-
   await page.screenshot({ path: './images/before.png' })
+
+  expect(await page.title).toBe("Symetrix - Inspired AV Processing & Control Solutions")
 
   await page.click("button:has-text('Validate')")
 
   await page.screenshot({ path: './images/after.png' })
-
-  await page.close()
 });
